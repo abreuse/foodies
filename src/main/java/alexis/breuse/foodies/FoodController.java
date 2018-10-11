44e2915187c39dd65f2ee4a8e10bae88063546b8
Alexis <alexis.breuse@gmail.com>
2018-10-10 23:07:57 -0400

    POST method to add a new Food

--- src/main/java/alexis/breuse/foodies/FoodController.java
+++ src/main/java/alexis/breuse/foodies/FoodController.java
@@ -3,10 +3,10 @@ package alexis.breuse.foodies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("food")
@@ -25,5 +25,18 @@ public class FoodController {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 
         return ResponseEntity.ok(foodRepository.findByName(name));
+    }
+
+    @PostMapping
+    public ResponseEntity postFood(@RequestBody Food food) throws URISyntaxException {
+        if(food.getName() == null || food.getName().equals(""))
+            return ResponseEntity.badRequest().body("Must have the property 'name'.");
+
+        else if(food.getCalories() == 0)
+            return ResponseEntity.badRequest().body("Must have the property 'calories'.");
+
+        foodRepository.save(food);
+
+        return ResponseEntity.created(new URI("localhost:8080/food?search=" + food.getName())).build();
     }
 }
